b556e1761b8ec9f61b6012d2c44280fd2aab4921
aurelionsulll <mennanamehdi@gmail.com>
2021-03-01 20:54:00 +0100

    3 commit

--- src/main/java/org/example/SecondaryController.java
+++ src/main/java/org/example/SecondaryController.java
@@ -1,12 +1,11 @@
-package org.example;
-
-import java.io.IOException;
-import javafx.fxml.FXML;
-
-public class SecondaryController {
-
-    @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
-    }
-}
+//package org.example;
+//
+//import java.io.IOException;
+//import javafx.fxml.FXML;
+//
+//
+//    @FXML
+//    private void switchToPrimary() throws IOException {
+//        App.setRoot("primary");
+//    }
+//}
