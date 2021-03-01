b0b0f076a525add658a45e74681fe997c93fb046
ziko-23 <57302647+ziko-23@users.noreply.github.com>
2021-03-01 22:14:03 +0100

    tesst

--- src/main/java/org/example/SecondaryController.java
+++ src/main/java/org/example/SecondaryController.java
@@ -1,11 +1,11 @@
-//package org.example;
-//
-//import java.io.IOException;
-//import javafx.fxml.FXML;
-//
-//
-//    @FXML
-//    private void switchToPrimary() throws IOException {
-//        App.setRoot("primary");
-//    }
-//}
+package org.example;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
